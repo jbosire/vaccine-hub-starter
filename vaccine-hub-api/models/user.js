147cc28f9d9ae9c76161521815157317603237c4
Joram Bosire <jbosire@jbosire-ltm73m6.internal.salesforce.com>
2022-07-06 14:58:38 -0700

    finalise on lab

--- vaccine-hub-api/models/user.js
+++ vaccine-hub-api/models/user.js
@@ -4,7 +4,36 @@ const {BCRYPT_WORK_FACTOR} = require("../config")
 const {BadRequestError,UnauthorizedError} = require("../utils/errors")
 
 class User {
+
+    static async makePublicUser(user){
+        return{
+            id: user.id,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email,
+            date: user.date
+
+
+        }
+    }
+
     static async login(credentials){
+        const requiredFields = ["email","password"]
+        requiredFields.forEach(field => {
+            if(!credentials.hasOwnProperty(field)){
+                throw new BadRequestError(`Missing ${field} in request body.`)
+            }
+        })
+
+        const user = await User.fetchUserByEmail(credentials.email)
+
+        if(user){
+            const isValid = await bcrypt.compare(credentials.password, user.password);
+            if(isValid){
+                return User.makePublicUser(user)
+            }
+        }
+
         
         throw new UnauthorizedError("Invalid email/password combo")
 
@@ -49,7 +78,7 @@ class User {
 
         const user = result.rows[0]
 
-        return user;
+        return User.makePublicUser(user);
 
     }
 
